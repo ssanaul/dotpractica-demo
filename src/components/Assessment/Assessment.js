890d03742a571432334325d58ed7a3c5e53a56a7
ssanaul <ss7@illinois.edu>
2017-11-01 15:46:38 -0500

    working build (before express)

--- src/components/Assessment/Assessment.js
+++ src/components/Assessment/Assessment.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import { CardTitle, Paper } from 'material-ui';
+import { Paper } from 'material-ui';
+import {Toolbar, ToolbarTitle} from 'material-ui/Toolbar';
 
 class Assessment extends Component { render() {
-
+  const styles = {
+    toolbar : {
+      marginTop: 20,
+      backgroundColor: 'rgba(200,200,200,.25)',
+    },
+    toolbarText : {
+      textAlign: 'center',
+    },
+    toolbarTextWrapper : {
+      width: '100%',
+      margin: 'auto',
+    },
+    problemWrapper : {
+      padding: 20,
+    },
+  }
   return ( <div>
 
-        <CardTitle title={this.props.title} style={{textAlign: 'center'}} />
-        
+        <Toolbar style={styles.toolbar}>
+            <ToolbarTitle text={
+              <p style={styles.toolbarText}>{this.props.title}</p>
+            }
+            style={styles.toolbarTextWrapper}/>
+        </Toolbar>
+
         <div>
           {
             this.props.problems.map(function(p){
               return <Paper
                       zDepth={1}
-                      style={{padding: 20}}>
+                      style={styles.problemWrapper}>
                         {p}
                     </Paper>
             })
--- src/components/Explanation/Explanation.js
+++ src/components/Explanation/Explanation.js
@@ -1,60 +1,80 @@
 import React, { Component } from 'react';
-import TextField from 'material-ui/TextField';
 import { Paper } from 'material-ui';
+import { List } from 'material-ui/List';
+import Chip from 'material-ui/Chip';
+import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
 
 class Explanation extends Component {
 	constructor(props){
 		super(props);
 
 		this.state = {
-			explanations : [],
-			currentIndex: 0,
+			chips : [],
 		}
-
-		this.addInput = this.addInput.bind(this);
 	}
-
-	addInput() {
-		var newIndex = this.state.currentIndex + 1;
-		var updatedExplanations = this.state.explanations;
-
-		var newExplanation = {
-			key: newIndex,
-			el: <div key={newIndex}>
-					<span>{newIndex+1}.</span>
-					<TextField/>
-				</div>,
-		}
-		updatedExplanations.push(newExplanation);
-
-		this.setState({
-			explanations: updatedExplanations,
-			currentIndex: newIndex,
-		});
-	}
-
   render() {
+		const styles = {
+			toolbar : {
+				height: 33,
+			},
+			toolbarText : {
+				fontSize: '12px',
+			},
+			customTextarea : {
+				width: '100%',
+				maxWidth: '100%',
+				minWidth: '100%',
+				height: '100%',
+				minHeight: 100,
+				border: 'none',
+				borderRight: '2px solid rgba(200,200,200,.5)',
+				borderBottom: '2px solid rgba(200,200,200,.5)',
+				outline: 'none',
+				overflow: 'auto',
+			},
+			nodeList : {
+				display: 'inline',
+				padding: 5,
+				marginLeft: 5,
+			},
+		}
     return (
       <div>
 			<h4>Explain:</h4>
 				<Paper>
 				<div className="row">
-					<div className="col-sm-9" style={{paddingRight: 0}}>
-						<textarea style={{
-							width: '100%',
-							maxWidth: '100%',
-							minWidth: '100%',
-							height: 100,
-							border: 'none',
-							borderRight: '2px solid rgba(200,200,200,.5)',
-							outline: 'none',
-							overflow: 'auto',
-						}}>
+					<div className="col-sm-12">
+						<Toolbar style={styles.toolbar}>
+							<ToolbarGroup>
+								<ToolbarTitle text={
+									<span style={styles.toolbarText}>
+										Options
+									</span>}
+								/>
+							</ToolbarGroup>
+						</Toolbar>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-12">
+						<textarea style={styles.customTextarea}>
 						</textarea>
 					</div>
-					<div className="col-sm-3" style={{paddingLeft: 3}}>
-						<span>Nodes used:</span>
-						</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-12">
+						<List>
+								<Chip style={styles.nodeList}>
+									item1
+								</Chip>
+								<Chip style={styles.nodeList}>
+									item2
+								</Chip>
+								<Chip style={styles.nodeList}>
+									item3
+								</Chip>
+						</List>
+					</div>
 				</div>
 				</Paper>
       </div>
